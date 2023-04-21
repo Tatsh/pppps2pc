d938b582f7051be4ad4735112586e39c15059258
Andrew Udvare <audvare@gmail.com>
2023-04-21 17:42:04 -0400

    kernel/hid-konami-ppp: add resume hooks

--- kernel/hid-konami-ppp.c
+++ kernel/hid-konami-ppp.c
@@ -15,8 +15,7 @@
 #define VENDOR_HOSIDEN 0x0507
 #define DEV_KONAMI_PPP 0x0011
 
-static int ppp_input_configured(struct hid_device *hdev,
-				struct hid_input *hidinput)
+static int ppp_send_output_report(struct hid_device *hdev)
 {
 	int ret = 0;
 	const int buf_size = 2;
@@ -26,7 +25,20 @@ static int ppp_input_configured(struct hid_device *hdev,
 	ret = hid_hw_raw_request(hdev, 0, buf, buf_size, HID_OUTPUT_REPORT,
 				 HID_REQ_SET_REPORT);
 	kfree(buf);
-	return ret != 2 ? 1 : 0;
+	return ret != buf_size ? 1 : 0;
+}
+
+#ifdef CONFIG_PM
+static int ppp_resume(struct hid_device *hdev)
+{
+	return ppp_send_output_report(hdev);
+}
+#endif
+
+static int ppp_input_configured(struct hid_device *hdev,
+				struct hid_input *hidinput)
+{
+	return ppp_send_output_report(hdev);
 }
 
 static const struct hid_device_id ppp_devices[] = {
@@ -36,10 +48,15 @@ static const struct hid_device_id ppp_devices[] = {
 
 MODULE_DEVICE_TABLE(hid, ppp_devices);
 
-static struct hid_driver ppp_driver = { .name = "konami-ppp",
-					.id_table = ppp_devices,
-					.input_configured =
-						ppp_input_configured };
+static struct hid_driver ppp_driver = {
+	.name = "konami-ppp",
+	.id_table = ppp_devices,
+	.input_configured = ppp_input_configured,
+#ifdef CONFIG_PM
+	.resume = ppp_resume,
+	.reset_resume = ppp_resume,
+#endif
+};
 
 module_hid_driver(ppp_driver);
 
