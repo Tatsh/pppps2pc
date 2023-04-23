59371e5d244b1c6f45b8cbecf0e11bca19b4a908
moversi <129067651+movrsi@users.noreply.github.com>
2023-04-23 17:34:02 -0400

    Refactor probe.c
    Refactored with how attach.c was done like.

--- ps2-decompile/probe.c
+++ ps2-decompile/probe.c
@@ -2,42 +2,55 @@
 #include <usbd.h>
 
 int usbmouse_probe(int devId) {
-    int result;                    // $v0
     UsbDeviceDescriptor *hdesc;    // [sp+10h] [+10h]
     UsbInterfaceDescriptor *idesc; // [sp+14h] [+14h]
 
+#ifdef DEBUG
     printf("-------- dev_id = %d\n", devId);
-    hdesc = sceUsbdScanStaticDescriptor(devId, 0, 1);
-    if (hdesc) {
-        idesc = sceUsbdScanStaticDescriptor(devId, hdesc, 4);
-        if (idesc) {
-            if (hdesc->bLength == 18 && hdesc->bDescriptorType == 1 && hdesc->bcdUSB == 272 &&
-                !hdesc->bDeviceClass && !hdesc->bDeviceSubClass && !hdesc->bDeviceProtocol &&
-                hdesc->bMaxPacketSize0 == 8 && hdesc->idVendor == 1287 && hdesc->idProduct == 17 &&
-                hdesc->iManufacturer == 1 && hdesc->iProduct == 2 && !hdesc->iSerialNumber &&
-                hdesc->bNumConfigurations == 1) {
-                if (idesc->bLength == 9 && idesc->bDescriptorType == 4 &&
-                    !idesc->bInterfaceNumber && !idesc->bAlternateSetting &&
-                    idesc->bNumEndpoints == 1 && idesc->bInterfaceClass == 3 &&
-                    !idesc->bInterfaceSubClass && !idesc->bInterfaceProtocol &&
-                    idesc->iInterface == 2) {
-                    printf("USB_PROBE END\n");
-                    result = 1;
-                } else {
-                    printf("Wrong Interface Desc.\n");
-                    result = 0;
-                }
-            } else {
-                printf("Wrong Device Desc.\n");
-                result = 0;
-            }
-        } else {
-            printf("#### USB : ScanStaticDescriptor Failed(1)\n");
-            result = 0;
-        }
-    } else {
+#endif
+
+    if ((hdesc = sceUsbdScanStaticDescriptor(devId, 0, 1)) == NULL) {
+#ifdef DEBUG
         printf("#### USB : ScanStaticDescriptor Failed(0)\n");
-        result = 0;
+#endif
+        goto fail;
     }
-    return result;
+
+    if ((idesc = sceUsbdScanStaticDescriptor(devId, hdesc, 4)) == NULL) {
+#ifdef DEBUG
+        printf("#### USB : ScanStaticDescriptor Failed(1)\n");
+#endif
+        goto fail;
+    }
+
+    if ((hdesc->bLength != 18 || hdesc->bDescriptorType != 1 || hdesc->bcdUSB != 272 ||
+        hdesc->bDeviceClass != 0 || hdesc->bDeviceSubClass >= 1 || hdesc->bDeviceProtocol != 0 ||
+        hdesc->bMaxPacketSize0 != 8 || hdesc->idVendor != 1287 || hdesc->idProduct != 17 ||
+        hdesc->iManufacturer != 1 || hdesc->iProduct != 2 || hdesc->iSerialNumber >= 1 ||
+        hdesc->bNumConfigurations != 1)) {
+#ifdef DEBUG
+        printf("Wrong Device Desc.\n");
+#endif
+        goto fail;
+    }
+
+    if (idesc->bLength != 9 || idesc->bDescriptorType != 4 ||
+        idesc->bInterfaceNumber != 0 || idesc->bAlternateSetting != 0 ||
+        idesc->bNumEndpoints != 1 || idesc->bInterfaceClass != 3 ||
+        idesc->bInterfaceSubClass != 0 || idesc->bInterfaceProtocol != 0 ||
+        idesc->iInterface != 2) {
+#ifdef DEBUG
+        printf("Wrong Interface Desc.\n");
+#endif
+        goto fail;
+    }
+
+#ifdef DEBUG
+    printf("USB_PROBE END\n");
+#endif
+
+    return 1;
+
+fail:
+    return 0;
 }
